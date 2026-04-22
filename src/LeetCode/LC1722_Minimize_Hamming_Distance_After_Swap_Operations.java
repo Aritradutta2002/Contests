d821325e0c39939366c196a0c6cf6a63aa80a283
Aritradutta2002 <aritradutta049@gmail.com>
2026-04-23 01:45:05 +0530

    feat: update .gitignore and add solution for Words Within Two Edits of Dictionary problem

--- src/LeetCode/LC1722_Minimize_Hamming_Distance_After_Swap_Operations.java
+++ src/LeetCode/LC1722_Minimize_Hamming_Distance_After_Swap_Operations.java
@@ -3,11 +3,18 @@ import java.util.*;
 /**
  * 1722. Minimize Hamming Distance After Swap Operations
  * 
- * You are given two integer arrays, source and target, both of length n. You are also given an array allowedSwaps where each allowedSwaps[i] = [ai, bi] indicates that you are allowed to swap the elements at index ai and index bi (0-indexed) of array source. Note that you can swap elements at a specific pair of indices multiple times and in any order.
+ * You are given two integer arrays, source and target, both of length n.
+ * You are also given an array allowedSwaps where each allowedSwaps[i] = [ai, bi]
+ * indicates that you are allowed to swap the elements at index ai and index bi
+ * (0-indexed) of array source. Note that you can swap elements at a specific pair
+ * of indices multiple times and in any order.
  * 
- * The Hamming distance of two arrays of the same length, source and target, is the number of positions where the elements are different. Formally, it is the number of indices i for 0 <= i <= n-1 where source[i] != target[i] (0-indexed).
+ * The Hamming distance of two arrays of the same length, source and target, is
+ * the number of positions where the elements are different. Formally, it is the
+ * number of indices i for 0 <= i <= n-1 where source[i] != target[i] (0-indexed).
  * 
- * Return the minimum Hamming distance of source and target after performing any amount of swap operations on array source.
+ * Return the minimum Hamming distance of source and target after performing any
+ * amount of swap operations on array source.
  * 
  * Example 1:
  * Input: source = [1,2,3,4], target = [2,1,4,5], allowedSwaps = [[0,1],[2,3]]
