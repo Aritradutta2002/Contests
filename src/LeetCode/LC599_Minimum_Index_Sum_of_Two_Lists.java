9aa98a00d4fec3e8dfc73d80ff78006aa71e3ccd
Aritradutta2002 <aritradutta049@gmail.com>
2026-05-02 02:13:43 +0530

    Update LC599_Minimum_Index_Sum_of_Two_Lists.java

--- src/LeetCode/LC599_Minimum_Index_Sum_of_Two_Lists.java
+++ src/LeetCode/LC599_Minimum_Index_Sum_of_Two_Lists.java
@@ -1,6 +1,5 @@
 package LeetCode;
 import java.util.*;
-
 /*
  * Problem 599: Minimum Index Sum of Two Lists
  * 
@@ -21,7 +20,23 @@ import java.util.*;
 public class LC599_Minimum_Index_Sum_of_Two_Lists {
 
     public String[] findRestaurant(String[] list1, String[] list2) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        List<String> list = new ArrayList<>();
+        int minimum = Integer.MAX_VALUE;
+        for(int i = 0; i < list1.length; i++){
+            for(int j = 0; j < list2.length; j++){
+                if(list1[i].equals(list2[j])){
+                    minimum = Math.min(minimum, i + j);
+                }
+            }
+        }
+        for(int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j]) && (i + j) <= minimum) {
+                    list.add(list1[i]);
+                }
+            }
+        }
+        return list.toArray(new String[0]);
     }
 
     public static void main(String[] args) {
